fbe17484e229b54be50c60c03cbc625e839b2e89
Zapperer04 <buddiekaustav@gmail.com>
2026-04-06 01:50:58 +0530

    Fix missing blueprint and jwt imports

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,3 +1,5 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, get_jwt
 from models import db, User
 
 auth_bp = Blueprint("auth_bp", __name__)
